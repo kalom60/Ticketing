6cba5bfea01257ab9be19c4ba340046b1c4d78a8
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-05 23:07:10 +0300

    ticket route added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import authRouter from "./auth";
+import ticketRouter from "./tickets";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/ticket", ticketRouter);
 
 export default router;
