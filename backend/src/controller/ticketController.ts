4a8c0f3f713c2eee6807a31796cfc4a2a4f4f84a
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-05 23:30:21 +0300

    put controller

--- backend/src/controller/ticketController.ts
+++ backend/src/controller/ticketController.ts
@@ -39,6 +39,27 @@ class TicketController {
       next(error);
     }
   }
+
+  static async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status } = req.body;
+      const { id } = req.params;
+
+      const ticket = await Ticket.findByIdAndUpdate(
+        id,
+        { status },
+        { new: true },
+      );
+
+      if (!ticket) {
+        return res.status(404).json({ message: "Ticket not found" });
+      }
+
+      return res.status(200).json(ticket);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default TicketController;
