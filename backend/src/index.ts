9f852144ecb451ee50e9618365f027bc2b5d209e
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-05 22:28:49 +0300

    import base route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db";
 import { setupSwagger } from "./config/swagger";
 import passport from "passport";
+import baseRouter from "./routes/index";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ app.use(express.json());
 setupSwagger(app);
 
 app.get("/", (req, res) => res.send("API is running... 🚀"));
+
+app.use("/api/v1", baseRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
