107733371efab3516a0d6cbd2194794d8bd770a8
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-05 22:48:06 +0300

    use Error type

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export const errorHandler = (err: Error, req: Request, res: Response) => {
+export const errorHandler = (err: Error, _: Request, res: Response) => {
   console.error(err);
 
   res.status(500).json({
