0069499ee50e56be3e2406a43a23b4b0611a42fd
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-05 23:09:18 +0300

    post ticket contoller

---
+++ backend/src/controller/ticketController.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import Ticket from "../models/Ticket";
+
+export interface AuthenticatedUser {
+  id: string;
+  role: string;
+  email: string;
+}
+
+class TicketController {
+  static async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { title, description, status } = req.body;
+      const user = req.user as { id: string };
+
+      const ticket = new Ticket({
+        title,
+        description,
+        status,
+        user: user.id,
+      });
+
+      await ticket.save();
+      return res.status(201).json(ticket);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default TicketController;
