232ab997888d7db2134ee55bd183c6c6a23a71d3
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-05 22:48:47 +0300

    use error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import connectDB from "./config/db";
 import { setupSwagger } from "./config/swagger";
 import passport from "passport";
 import baseRouter from "./routes/index";
+import { errorHandler } from "./middlewares/errorHandler";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,15 +13,28 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*", // Specify the allowed origin
+    methods: "GET,POST,PUT,DELETE, PATCH",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(passport.initialize());
 import "./config/passport";
 
 app.use(express.json());
 setupSwagger(app);
 
-app.get("/", (req, res) => res.send("API is running... 🚀"));
-
 app.use("/api/v1", baseRouter);
+
+app.use(errorHandler);
+
+app.get("/", (_, res) => res.send("API is running... 🚀"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
