4ad58ae6b526988cc7b2a128ffcd55ce9cf1fa94
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-06 17:03:18 +0300

    sign up page

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import SignupForm from "../components/SignupForm";
+
+export class Signup extends Component {
+  render() {
+    return (
+      <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+        <div className="w-full max-w-sm md:max-w-3xl">
+          <SignupForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
