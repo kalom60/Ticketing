ffab42be77dcbdf85700cc9bbfb89ff5915ef42d
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-06 12:09:58 +0300

    login form

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,62 @@
+import { Component } from "react";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+export class LoginForm extends Component {
+  render() {
+    return (
+      <div className="flex flex-col gap-6">
+        <Card>
+          <CardHeader className="flex flex-col items-center text-center">
+            <CardTitle className="text-2xl font-bold">
+              Welcome back to Ticketing System
+            </CardTitle>
+            <CardDescription className="text-balance text-muted-foreground">
+              Login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
