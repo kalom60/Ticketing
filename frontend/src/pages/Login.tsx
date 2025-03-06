d464c73ec5aa65b064c07f5a804d414b7ce7f69b
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-03-06 17:40:00 +0300

    login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import LoginForm from "../components/LoginForm";
+
+export class Login extends Component {
+  render() {
+    return (
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <LoginForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
